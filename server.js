3c19b594fbf791eee843e83890120b7cd8013281
Ben Zulauf <bezulauf@microsoft.com>
2016-02-10 18:59:58 -0800

    port

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var oxford = require('project-oxford')
 var client = new oxford.Client('c6a56dbc2117468ba20aed22858b8454');
 
+var port = process.env.PORT || 9999;
+
 client.face.detect({
     path: 'public/images/lebron.jpg',
     analyzesAge: true,
@@ -17,6 +19,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(9999, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 9999!');
 });
